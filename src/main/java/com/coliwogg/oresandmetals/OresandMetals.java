55184f7a32714985d05d256c14e31002b22c5043
Colin <coliwogg@outlook.com>
2020-04-04 12:21:15 -0300

    Initial setup complete

---
+++ src/main/java/com/coliwogg/oresandmetals/OresandMetals.java
@@ -0,0 +1,32 @@
+package com.coliwogg.oresandmetals;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("oresandmetals")
+public class OresandMetals {
+
+    public OresandMetals() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) {
+
+    }
+}
