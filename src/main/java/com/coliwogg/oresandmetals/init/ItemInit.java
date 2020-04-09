20f0c773637a13c769b15a9344f28522b5ccad5a
Colin <coliwogg@outlook.com>
2020-04-09 11:22:14 -0300

    Added Mod Item Tool Values

--- src/main/java/com/coliwogg/oresandmetals/init/ItemInit.java
+++ src/main/java/com/coliwogg/oresandmetals/init/ItemInit.java
@@ -1,9 +1,14 @@
 package com.coliwogg.oresandmetals.init;
 
+import java.util.function.Supplier;
+
 import com.coliwogg.oresandmetals.OresandMetals;
 import com.coliwogg.oresandmetals.OresandMetals.OresandMetalsItemGroup;
 
+import net.minecraft.item.IItemTier;
 import net.minecraft.item.Item;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -33,7 +38,14 @@ public class ItemInit {
 	public static final Item RUNITE_FRAGMENTS = null;
 	public static final Item RUNITE_CHUNK = null;
 	public static final Item RUNITE_INGOT = null;
-
+	
+	// Bronze Tools
+	public static final Item BRONZE_AXE = null;
+	public static final Item BRONZE_HOE = null;
+	public static final Item BRONZE_PICKAXE = null;
+	public static final Item BRONZE_SHOVEL = null;
+	public static final Item BRONZE_SWORD = null;
+	
 	@SubscribeEvent
 	public static void registerItems(final RegistryEvent.Register<Item> event) {
 		event.getRegistry().register(new Item(new Item.Properties().group(OresandMetalsItemGroup.instance)).setRegistryName("copper_fragments"));
@@ -55,5 +67,60 @@ public class ItemInit {
 		event.getRegistry().register(new Item(new Item.Properties().group(OresandMetalsItemGroup.instance)).setRegistryName("runite_fragments"));
 		event.getRegistry().register(new Item(new Item.Properties().group(OresandMetalsItemGroup.instance)).setRegistryName("runite_chunk"));
 		event.getRegistry().register(new Item(new Item.Properties().group(OresandMetalsItemGroup.instance)).setRegistryName("runite_ingot"));
+
+	}
+	
+	public enum ModItemTier implements IItemTier {
+		BRONZE(200, 5.0F, 1.5F, 1, 15, () -> { return Ingredient.fromItems(ItemInit.BRONZE_INGOT); }),
+		STEEL(350, 6.5F, 2.5F, 2, 14, () -> { return Ingredient.fromItems(ItemInit.STEEL_INGOT); }),
+		MITHRIL(525, 7.0F, 2.625F, 2, 12, () -> { return Ingredient.fromItems(ItemInit.MITHRIL_INGOT); }),
+		ADAMANTITE(600, 7.5F, 2.75F, 2, 11, () -> { return Ingredient.fromItems(ItemInit.ADAMANTITE_INGOT); }),
+		RUNITE(1750, 10.0F, 4.0F, 3, 10, () -> { return Ingredient.fromItems(ItemInit.RUNITE_INGOT); });
+
+		private final int maxUses;
+		private final float efficiency;
+		private final float attackDamage;
+		private final int harvestLevel;
+		private final int enchantability;
+		private final LazyValue<Ingredient> repairMaterial;
+		
+		private ModItemTier(int maxUses, float efficiency, float attackDamage, int harvestLevel, int enchantability, Supplier<Ingredient> repairMaterial) {
+			this.maxUses = maxUses;
+			this.efficiency = efficiency;
+			this.attackDamage = attackDamage;
+			this.harvestLevel = harvestLevel;
+			this.enchantability = enchantability;
+			this.repairMaterial = new LazyValue<>(repairMaterial);
+		}
+
+		@Override
+		public int getMaxUses() {
+			return this.maxUses;
+		}
+
+		@Override
+		public float getEfficiency() {
+			return this.efficiency;
+		}
+
+		@Override
+		public float getAttackDamage() {
+			return this.attackDamage;
+		}
+
+		@Override
+		public int getHarvestLevel() {
+			return this.harvestLevel;
+		}
+
+		@Override
+		public int getEnchantability() {
+			return this.enchantability;
+		}
+
+		@Override
+		public Ingredient getRepairMaterial() {
+			return this.repairMaterial.getValue();
+		}
 	}
 }
