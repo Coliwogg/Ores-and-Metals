2f262fe4c69da0b6f4fd799d05b3bc9452d0f59f
Colin <coliwogg@outlook.com>
2020-04-10 15:06:43 -0300

    Added and implemented Bronze Armor

--- src/main/java/com/coliwogg/oresandmetals/init/ItemInit.java
+++ src/main/java/com/coliwogg/oresandmetals/init/ItemInit.java
@@ -4,9 +4,13 @@ import java.util.function.Supplier;
 
 import com.coliwogg.oresandmetals.OresandMetals;
 import com.coliwogg.oresandmetals.OresandMetals.OresandMetalsItemGroup;
+import com.coliwogg.oresandmetals.init.ItemInit.ModItemTier.ModArmorMaterial;
 
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.AxeItem;
 import net.minecraft.item.HoeItem;
+import net.minecraft.item.IArmorMaterial;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.Item;
 import net.minecraft.item.PickaxeItem;
@@ -14,6 +18,10 @@ import net.minecraft.item.ShovelItem;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.LazyValue;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -79,6 +87,12 @@ public class ItemInit {
 	public static final Item RUNITE_SHOVEL = null;
 	public static final Item RUNITE_SWORD = null;
 	
+	// Bronze Armor
+	public static final Item BRONZE_HELMET = null;
+	public static final Item BRONZE_CHESTPLATE = null;
+	public static final Item BRONZE_LEGGINGS = null;
+	public static final Item BRONZE_BOOTS = null;
+	
 	@SubscribeEvent
 	public static void registerItems(final RegistryEvent.Register<Item> event) {
 		event.getRegistry().register(new Item(new Item.Properties().group(OresandMetalsItemGroup.instance)).setRegistryName("copper_fragments"));
@@ -130,6 +144,11 @@ public class ItemInit {
 		event.getRegistry().register(new PickaxeItem(ModItemTier.RUNITE, 3, -2.8F, new Item.Properties().group(OresandMetalsItemGroup.instance)).setRegistryName("runite_pickaxe"));
 		event.getRegistry().register(new ShovelItem(ModItemTier.RUNITE, 3.5F, -3.0F, new Item.Properties().group(OresandMetalsItemGroup.instance)).setRegistryName("runite_shovel"));
 		event.getRegistry().register(new SwordItem(ModItemTier.RUNITE, 5, -2.4F, new Item.Properties().group(OresandMetalsItemGroup.instance)).setRegistryName("runite_sword"));
+		
+		event.getRegistry().register(new ArmorItem(ModArmorMaterial.BRONZE, EquipmentSlotType.HEAD, new Item.Properties().group(OresandMetalsItemGroup.instance)).setRegistryName("bronze_helmet"));
+		event.getRegistry().register(new ArmorItem(ModArmorMaterial.BRONZE, EquipmentSlotType.CHEST, new Item.Properties().group(OresandMetalsItemGroup.instance)).setRegistryName("bronze_chestplate"));
+		event.getRegistry().register(new ArmorItem(ModArmorMaterial.BRONZE, EquipmentSlotType.LEGS, new Item.Properties().group(OresandMetalsItemGroup.instance)).setRegistryName("bronze_leggings"));
+		event.getRegistry().register(new ArmorItem(ModArmorMaterial.BRONZE, EquipmentSlotType.FEET, new Item.Properties().group(OresandMetalsItemGroup.instance)).setRegistryName("bronze_boots"));
 
 	}
 	
@@ -184,6 +203,69 @@ public class ItemInit {
 		@Override
 		public Ingredient getRepairMaterial() {
 			return this.repairMaterial.getValue();
+		}
+		
+		public enum ModArmorMaterial implements IArmorMaterial {
+			BRONZE(10, new int[] { 1, 4, 5, 2 }, 12, "item.armor.equip_iron", OresandMetals.MODID + ":bronze", 0.0F, () -> { return Ingredient.fromItems(ItemInit.BRONZE_INGOT); }),
+			STEEL(20, new int[] { 2, 6, 7, 2 }, 12, "item.armor.equip_iron", OresandMetals.MODID + ":steel", 0.0F, () -> { return Ingredient.fromItems(ItemInit.STEEL_INGOT); }),
+			MITHRIL(25, new int[] { 3, 6, 8, 2 }, 12, "item.armor.equip_iron", OresandMetals.MODID + ":mithril", 1.0F, () -> { return Ingredient.fromItems(ItemInit.MITHRIL_INGOT); }),
+			ADAMANTITE(30, new int[] { 2, 7, 8, 3 }, 12, "item.armor.equip_iron", OresandMetals.MODID + ":adamantite", 1.0F, () -> { return Ingredient.fromItems(ItemInit.ADAMANTITE_INGOT); }),
+			RUNITE(40, new int[] { 4, 7, 9, 4 }, 12, "item.armor.equip_iron", OresandMetals.MODID + ":runite", 3.0F, () -> { return Ingredient.fromItems(ItemInit.RUNITE_INGOT); });
+
+			private static final int[] MAX_DAMAGE_ARRAY = new int[] {16, 16, 16, 16};
+			private final int durability;
+			private final int[] damageReductionAmount;
+			private final int enchantability;
+			private final String soundEvent;
+			private final LazyValue<Ingredient> repairMaterial;
+			private final String name;
+			private final float toughness;
+			
+			private ModArmorMaterial(int durability, int[] damageReductionAmount, int enchantability, String soundEvent, String name, float toughness, Supplier<Ingredient> repairMaterial) {
+				this.durability = durability;
+				this.damageReductionAmount = damageReductionAmount;
+				this.enchantability = enchantability;
+				this.soundEvent = soundEvent;
+				this.name = name;
+				this.toughness = toughness;
+				this.repairMaterial = new LazyValue<>(repairMaterial);
+			}
+
+			@Override
+			public int getDurability(EquipmentSlotType slotIn) {
+				return MAX_DAMAGE_ARRAY[slotIn.getIndex()] * this.durability;
+			}
+
+			@Override
+			public int getDamageReductionAmount(EquipmentSlotType slotIn) {
+				return this.damageReductionAmount[slotIn.getIndex()];
+			}
+
+			@Override
+			public int getEnchantability() {
+				return this.enchantability;
+			}
+
+			@Override
+			public SoundEvent getSoundEvent() {
+				return new SoundEvent(new ResourceLocation(this.soundEvent));
+			}
+			
+			@OnlyIn(Dist.CLIENT)
+			@Override
+			public String getName() {
+				return this.name;
+			}
+
+			@Override
+			public float getToughness() {
+				return this.toughness;
+			}
+
+			@Override
+			public Ingredient getRepairMaterial() {
+				return this.repairMaterial.getValue();
+			}
 		}
 	}
 }
