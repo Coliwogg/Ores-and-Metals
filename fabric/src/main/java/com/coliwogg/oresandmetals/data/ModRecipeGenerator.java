60eddb658132e7e2cb389529e307b876a23ad18a
Coliwogg <coliwogg@outlook.com>
2023-10-14 10:10:35 -0300

    Add tool and armor recipes to fabric

--- fabric/src/main/java/com/coliwogg/oresandmetals/data/ModRecipeGenerator.java
+++ fabric/src/main/java/com/coliwogg/oresandmetals/data/ModRecipeGenerator.java
@@ -86,6 +86,60 @@ public class ModRecipeGenerator extends FabricRecipeProvider {
                 .criterion(FabricRecipeProvider.hasItem(Items.COAL), FabricRecipeProvider.conditionsFromItem(Items.COAL))
                 .offerTo(exporter, new Identifier(FabricRecipeProvider.getRecipeName(ModItems.RUNITE_INGOT)));
 
+        offerSwordRecipe(exporter, ModItems.BRONZE_SWORD, ModItems.BRONZE_INGOT);
+        offerSwordRecipe(exporter, ModItems.STEEL_SWORD, ModItems.STEEL_INGOT);
+        offerSwordRecipe(exporter, ModItems.MITHRIL_SWORD, ModItems.MITHRIL_INGOT);
+        offerSwordRecipe(exporter, ModItems.ADAMANT_SWORD, ModItems.ADAMANTITE_INGOT);
+        offerSwordRecipe(exporter, ModItems.RUNE_SWORD, ModItems.RUNITE_INGOT);
+
+        offerShovelRecipe(exporter, ModItems.BRONZE_SHOVEL, ModItems.BRONZE_INGOT);
+        offerShovelRecipe(exporter, ModItems.STEEL_SHOVEL, ModItems.STEEL_INGOT);
+        offerShovelRecipe(exporter, ModItems.MITHRIL_SHOVEL, ModItems.MITHRIL_INGOT);
+        offerShovelRecipe(exporter, ModItems.ADAMANT_SHOVEL, ModItems.ADAMANTITE_INGOT);
+        offerShovelRecipe(exporter, ModItems.RUNE_SHOVEL, ModItems.RUNITE_INGOT);
+
+        offerPickaxeRecipe(exporter, ModItems.BRONZE_PICKAXE, ModItems.BRONZE_INGOT);
+        offerPickaxeRecipe(exporter, ModItems.STEEL_PICKAXE, ModItems.STEEL_INGOT);
+        offerPickaxeRecipe(exporter, ModItems.MITHRIL_PICKAXE, ModItems.MITHRIL_INGOT);
+        offerPickaxeRecipe(exporter, ModItems.ADAMANT_PICKAXE, ModItems.ADAMANTITE_INGOT);
+        offerPickaxeRecipe(exporter, ModItems.RUNE_PICKAXE, ModItems.RUNITE_INGOT);
+
+        offerHoeRecipe(exporter, ModItems.BRONZE_HOE, ModItems.BRONZE_INGOT);
+        offerHoeRecipe(exporter, ModItems.STEEL_HOE, ModItems.STEEL_INGOT);
+        offerHoeRecipe(exporter, ModItems.MITHRIL_HOE, ModItems.MITHRIL_INGOT);
+        offerHoeRecipe(exporter, ModItems.ADAMANT_HOE, ModItems.ADAMANTITE_INGOT);
+        offerHoeRecipe(exporter, ModItems.RUNE_HOE, ModItems.RUNITE_INGOT);
+
+        offerAxeRecipe(exporter, ModItems.BRONZE_AXE, ModItems.BRONZE_INGOT);
+        offerAxeRecipe(exporter, ModItems.STEEL_AXE, ModItems.STEEL_INGOT);
+        offerAxeRecipe(exporter, ModItems.MITHRIL_AXE, ModItems.MITHRIL_INGOT);
+        offerAxeRecipe(exporter, ModItems.ADAMANT_AXE, ModItems.ADAMANTITE_INGOT);
+        offerAxeRecipe(exporter, ModItems.RUNE_AXE, ModItems.RUNITE_INGOT);
+
+        offerHelmetRecipe(exporter, ModItems.BRONZE_HELMET, ModItems.BRONZE_INGOT);
+        offerHelmetRecipe(exporter, ModItems.STEEL_HELMET, ModItems.STEEL_INGOT);
+        offerHelmetRecipe(exporter, ModItems.MITHRIL_HELMET, ModItems.MITHRIL_INGOT);
+        offerHelmetRecipe(exporter, ModItems.ADAMANT_HELMET, ModItems.ADAMANTITE_INGOT);
+        offerHelmetRecipe(exporter, ModItems.RUNE_HELMET, ModItems.RUNITE_INGOT);
+
+        offerChestplateRecipe(exporter, ModItems.BRONZE_CHESTPLATE, ModItems.BRONZE_INGOT);
+        offerChestplateRecipe(exporter, ModItems.STEEL_CHESTPLATE, ModItems.STEEL_INGOT);
+        offerChestplateRecipe(exporter, ModItems.MITHRIL_CHESTPLATE, ModItems.MITHRIL_INGOT);
+        offerChestplateRecipe(exporter, ModItems.ADAMANT_CHESTPLATE, ModItems.ADAMANTITE_INGOT);
+        offerChestplateRecipe(exporter, ModItems.RUNE_CHESTPLATE, ModItems.RUNITE_INGOT);
+
+        offerLeggingsRecipe(exporter, ModItems.BRONZE_LEGGINGS, ModItems.BRONZE_INGOT);
+        offerLeggingsRecipe(exporter, ModItems.STEEL_LEGGINGS, ModItems.STEEL_INGOT);
+        offerLeggingsRecipe(exporter, ModItems.MITHRIL_LEGGINGS, ModItems.MITHRIL_INGOT);
+        offerLeggingsRecipe(exporter, ModItems.ADAMANT_LEGGINGS, ModItems.ADAMANTITE_INGOT);
+        offerLeggingsRecipe(exporter, ModItems.RUNE_LEGGINGS, ModItems.RUNITE_INGOT);
+
+        offerBootsRecipe(exporter, ModItems.BRONZE_BOOTS, ModItems.BRONZE_INGOT);
+        offerBootsRecipe(exporter, ModItems.STEEL_BOOTS, ModItems.STEEL_INGOT);
+        offerBootsRecipe(exporter, ModItems.MITHRIL_BOOTS, ModItems.MITHRIL_INGOT);
+        offerBootsRecipe(exporter, ModItems.ADAMANT_BOOTS, ModItems.ADAMANTITE_INGOT);
+        offerBootsRecipe(exporter, ModItems.RUNE_BOOTS, ModItems.RUNITE_INGOT);
+
         offerArrowRecipe(exporter, ModItems.BRONZE_ARROW, ModItems.BRONZE_INGOT);
         offerArrowRecipe(exporter, ModItems.IRON_ARROW, Items.IRON_INGOT);
         offerArrowRecipe(exporter, ModItems.STEEL_ARROW, ModItems.STEEL_INGOT);
@@ -101,6 +155,104 @@ public class ModRecipeGenerator extends FabricRecipeProvider {
     public static void offerReversibleCompactingRecipes(Consumer<RecipeJsonProvider> exporter, RecipeCategory reverseCategory, ItemConvertible baseItem, RecipeCategory compactingCategory, ItemConvertible compactItem, String compactingId, @Nullable String compactingGroup, String reverseId, @Nullable String reverseGroup) {
         ShapelessRecipeJsonBuilder.create(reverseCategory, baseItem, 9).input(compactItem).group(reverseGroup).criterion(RecipeProvider.hasItem(compactItem), RecipeProvider.conditionsFromItem(compactItem)).offerTo(exporter, new Identifier(reverseId + "_from_" + RecipeProvider.getItemPath(compactItem)));
         ShapedRecipeJsonBuilder.create(compactingCategory, compactItem).input(Character.valueOf('#'), baseItem).pattern("###").pattern("###").pattern("###").group(compactingGroup).criterion(RecipeProvider.hasItem(baseItem), RecipeProvider.conditionsFromItem(baseItem)).offerTo(exporter, new Identifier(compactingId));
+    }
+
+    public static void offerAxeRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, ItemConvertible input) {
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, output, 1)
+                .pattern("##")
+                .pattern("#|")
+                .pattern(" |")
+                .input('#', input)
+                .input('|', Items.STICK)
+                .criterion(hasItem(input), conditionsFromItem(input))
+                .criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
+                .offerTo(exporter, new Identifier(getRecipeName(output)));
+    }
+
+    public static void offerHoeRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, ItemConvertible input) {
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, output, 1)
+                .pattern("##")
+                .pattern(" |")
+                .pattern(" |")
+                .input('#', input)
+                .input('|', Items.STICK)
+                .criterion(hasItem(input), conditionsFromItem(input))
+                .criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
+                .offerTo(exporter, new Identifier(getRecipeName(output)));
+    }
+
+    public static void offerPickaxeRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, ItemConvertible input) {
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, output, 1)
+                .pattern("###")
+                .pattern(" | ")
+                .pattern(" | ")
+                .input('#', input)
+                .input('|', Items.STICK)
+                .criterion(hasItem(input), conditionsFromItem(input))
+                .criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
+                .offerTo(exporter, new Identifier(getRecipeName(output)));
+    }
+
+    public static void offerShovelRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, ItemConvertible input) {
+        ShapedRecipeJsonBuilder.create(RecipeCategory.TOOLS, output, 1)
+                .pattern("#")
+                .pattern("|")
+                .pattern("|")
+                .input('#', input)
+                .input('|', Items.STICK)
+                .criterion(hasItem(input), conditionsFromItem(input))
+                .criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
+                .offerTo(exporter, new Identifier(getRecipeName(output)));
+    }
+
+    public static void offerSwordRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, ItemConvertible input) {
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, output, 1)
+                .pattern("#")
+                .pattern("#")
+                .pattern("|")
+                .input('#', input)
+                .input('|', Items.STICK)
+                .criterion(hasItem(input), conditionsFromItem(input))
+                .criterion(hasItem(Items.STICK), conditionsFromItem(Items.STICK))
+                .offerTo(exporter, new Identifier(getRecipeName(output)));
+    }
+
+    public static void offerHelmetRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, ItemConvertible input) {
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, output, 1)
+                .pattern("###")
+                .pattern("# #")
+                .input('#', input)
+                .criterion(hasItem(input), conditionsFromItem(input))
+                .offerTo(exporter, new Identifier(getRecipeName(output)));
+    }
+
+    public static void offerChestplateRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, ItemConvertible input) {
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, output, 1)
+                .pattern("# #")
+                .pattern("###")
+                .pattern("###")
+                .input('#', input)
+                .criterion(hasItem(input), conditionsFromItem(input))
+                .offerTo(exporter, new Identifier(getRecipeName(output)));
+    }
+
+    public static void offerLeggingsRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, ItemConvertible input) {
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, output, 1)
+                .pattern("###")
+                .pattern("# #")
+                .pattern("# #")
+                .input('#', input)
+                .criterion(hasItem(input), conditionsFromItem(input))
+                .offerTo(exporter, new Identifier(getRecipeName(output)));
+    }
+
+    public static void offerBootsRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, ItemConvertible input) {
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, output, 1)
+                .pattern("# #")
+                .pattern("# #")
+                .input('#', input)
+                .criterion(hasItem(input), conditionsFromItem(input))
+                .offerTo(exporter, new Identifier(getRecipeName(output)));
     }
 
     private static void offerArrowRecipe(Consumer<RecipeJsonProvider> exporter, ItemConvertible output, ItemConvertible input) {
